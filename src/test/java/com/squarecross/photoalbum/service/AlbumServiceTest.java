b959995b60712f934da27099ec5f25e80ad195ff
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-03 21:42:10 +0900

    Create AlbumServiceTest.java

---
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -0,0 +1,31 @@
+package com.squarecross.photoalbum.service;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class AlbumServiceTest {
+
+    @Autowired
+    AlbumService albumService;
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Test
+    void getAlbum() {
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        Album savedAlbum = albumRepository.save(album);
+
+        Album resAlbum = albumService.getAlbum(savedAlbum.getAlumId());
+        assertEquals("테스트", resAlbum.getAlbumName());
+    }
+}
