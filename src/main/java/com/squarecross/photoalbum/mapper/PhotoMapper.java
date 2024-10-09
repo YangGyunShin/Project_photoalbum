ef4d6693e5485a57bdd7b192cbc13f1810291037
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-09 18:04:55 +0900

    Create PhotoMapper.java

---
+++ src/main/java/com/squarecross/photoalbum/mapper/PhotoMapper.java
@@ -0,0 +1,10 @@
+package com.squarecross.photoalbum.mapper;
+
+import com.squarecross.photoalbum.domain.Photo;
+import com.squarecross.photoalbum.dto.PhotoDto;
+
+public class PhotoMapper {
+//    public static PhotoDto converToDto(Photo photo) {
+//
+//    }
+}
