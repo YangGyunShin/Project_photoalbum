0194e9472e6ee2c63c5f45b4b6ae29c4759dd148
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-09 17:48:00 +0900

    Update AlbumService.java

--- src/main/java/com/squarecross/photoalbum/service/AlbumService.java
+++ src/main/java/com/squarecross/photoalbum/service/AlbumService.java
@@ -2,18 +2,24 @@ package com.squarecross.photoalbum.service;
 
 import com.squarecross.photoalbum.Constants;
 import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.domain.Photo;
 import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.mapper.AlbumMapper;
 import com.squarecross.photoalbum.repository.AlbumRepository;
 import com.squarecross.photoalbum.repository.PhotoRepository;
 import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.type.descriptor.java.ObjectJavaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AlbumService {
@@ -48,5 +54,33 @@ public class AlbumService {
         Files.createDirectories(Paths.get(Constants.PATH_PREFIX + "/photos/thumb/" + album.getAlbumId()));
     }
 
+    public List<AlbumDto> getAlbumList(String keyword, String sort) {
+        List<Album> albums;
+        if (Objects.equals(sort, "byName")) {
+            albums = albumRepository.findByAlbumNameContainingOrderByAlbumNameAsc(keyword);
+        } else if (Objects.equals(sort, "byDate")) {
+            albums = albumRepository.findByAlbumNameContainingOrderByCreatedAtDesc(keyword);
+        } else {
+            throw new IllegalArgumentException("알 수 없는 정렬 기준입니다.");
+        }
+        List<AlbumDto> albumDtos = AlbumMapper.convertToDtoList(albums);
 
+        for (AlbumDto albumDto : albumDtos) {
+            List<Photo> top4 = photoRepository.findTop4ByAlbum_AlbumIdOrderByUploadedAtDesc(albumDto.getAlbumId());
+            albumDto.setThumbUrls(top4.stream().map(Photo::getThumbUrl).map(c -> Constants.PATH_PREFIX + c).collect(Collectors.toList()));
+        }
+        return albumDtos;
+    }
+
+    public AlbumDto changeName(Long AlbumId, AlbumDto albumDto) {
+        Optional<Album> album = this.albumRepository.findById(AlbumId);
+        if (album.isEmpty()) {
+            throw new NoSuchElementException(String.format("Album Id '%d'가 존재하지 않습니다.", AlbumId));
+        }
+
+        Album updateAlbum = album.get();
+        updateAlbum.setAlbumName(albumDto.getAlbumName());
+        Album savedAlbum = this.albumRepository.save(updateAlbum);
+        return AlbumMapper.convertToDto(savedAlbum);
+    }
 }
