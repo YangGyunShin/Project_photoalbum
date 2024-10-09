5eb4ffee472e37a5d334e1a7e250fba7f22675d7
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-09 17:48:05 +0900

    Update AlbumRepository.java

--- src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -4,6 +4,12 @@ import com.squarecross.photoalbum.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    List<Album> findByAlbumNameContainingOrderByCreatedAtDesc(String keyword);
+
+    List<Album> findByAlbumNameContainingOrderByAlbumNameAsc(String keyword);
 }
