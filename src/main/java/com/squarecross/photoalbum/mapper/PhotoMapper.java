151de692873f6fd357cf4ac259a0c799aa124acf
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-12 14:05:40 +0900

    Update PhotoMapper.java

--- src/main/java/com/squarecross/photoalbum/mapper/PhotoMapper.java
+++ src/main/java/com/squarecross/photoalbum/mapper/PhotoMapper.java
@@ -1,10 +1,20 @@
 package com.squarecross.photoalbum.mapper;
 
+import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.domain.Photo;
+import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.dto.PhotoDto;
 
 public class PhotoMapper {
-//    public static PhotoDto converToDto(Photo photo) {
-//
-//    }
+
+    public static PhotoDto convertToDto(Photo photo) {
+
+        PhotoDto photoDto = new PhotoDto();
+        photoDto.setPhotoId(photo.getPhotoId());
+        photoDto.setFileName(photo.getFileName());
+        photoDto.setFileSize(photo.getFileSize());
+        photoDto.setOriginalUrl(photo.getOriginalUrl());
+        photoDto.setThumbUrl(photo.getThumbUrl());
+        return photoDto;
+    }
 }
