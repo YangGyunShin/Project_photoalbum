769573f2dba4f322fb3c410433b57252d65f98e0
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-12 14:05:48 +0900

    Update PhotoController.java

--- src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
+++ src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
@@ -1,9 +1,22 @@
 package com.squarecross.photoalbum.controller;
 
+import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.service.PhotoService;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.h2.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/albums/{albumId}/photos")
@@ -11,4 +24,39 @@ public class PhotoController {
 
     @Autowired
     private PhotoService photoService;
+
+    // 사진 상세정보 API
+    @RequestMapping(value = "/{photoId}", method = RequestMethod.GET)
+    public ResponseEntity<PhotoDto> getPhotoInfo(@PathVariable("photoId") final Long photoId) {
+        PhotoDto photoDto = photoService.getPhoto(photoId);
+        return new ResponseEntity<>(photoDto, HttpStatus.OK);
+    }
+
+    // 사진 업로드 API
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<List<PhotoDto>> uploadPhotos(@PathVariable("albumId") final Long albumId, @RequestParam("photos") MultipartFile[] files) throws IOException {
+        List<PhotoDto> photos = new ArrayList<>();
+        for (MultipartFile file : files) {
+            PhotoDto photoDto = photoService.savePhoto(file, albumId);
+            photos.add(photoDto);
+        }
+        return new ResponseEntity<>(photos, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void downloadPhotos(@RequestParam("photoIds") long[] photoIds, HttpServletResponse response) {
+        try {
+            if (photoIds.length == 1) {
+                File file = photoService.getImageFile(photoIds[0]);
+                ServletOutputStream outputStream = response.getOutputStream();
+                IOUtils.copy(new FileInputStream(file), outputStream);
+                outputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Error");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
