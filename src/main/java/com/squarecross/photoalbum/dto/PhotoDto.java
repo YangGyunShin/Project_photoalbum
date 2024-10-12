bc740a3c262f1d5333f9e3188a4c17f5fd49852b
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-12 14:05:44 +0900

    Update PhotoDto.java

--- src/main/java/com/squarecross/photoalbum/dto/PhotoDto.java
+++ src/main/java/com/squarecross/photoalbum/dto/PhotoDto.java
@@ -9,6 +9,63 @@ public class PhotoDto {
     private int fileSize;
     private String originalUrl;
     private String thumbUrl;
+
+    public Long getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(Long photoId) {
+        this.photoId = photoId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getThumbUrl() {
+        return thumbUrl;
+    }
+
+    public void setThumbUrl(String thumbUrl) {
+        this.thumbUrl = thumbUrl;
+    }
+
+    public Date getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(Date uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+
+    public Long getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Long albumId) {
+        this.albumId = albumId;
+    }
+
     private Date uploadedAt;
     private Long albumId;
 }
