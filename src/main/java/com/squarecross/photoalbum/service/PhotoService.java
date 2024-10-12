2ebb9e1717f8d0e29a4df6fef662d02dea1d3a19
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-12 14:05:10 +0900

    Update PhotoService.java

--- src/main/java/com/squarecross/photoalbum/service/PhotoService.java
+++ src/main/java/com/squarecross/photoalbum/service/PhotoService.java
@@ -1,9 +1,29 @@
 package com.squarecross.photoalbum.service;
 
+import com.squarecross.photoalbum.Constants;
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.domain.Photo;
+import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.dto.PhotoDto;
+import com.squarecross.photoalbum.mapper.AlbumMapper;
+import com.squarecross.photoalbum.mapper.PhotoMapper;
+import com.squarecross.photoalbum.repository.AlbumRepository;
 import com.squarecross.photoalbum.repository.PhotoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImagingOpException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @Service
 public class PhotoService {
@@ -11,7 +31,85 @@ public class PhotoService {
     @Autowired
     private PhotoRepository photoRepository;
 
-//    public PhotoDto getPhoto(Long PhotoId) {
-//
-//    }
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    private final String original_path = Constants.PATH_PREFIX + "/photos/original";
+    private final String thumb_path = Constants.PATH_PREFIX + "/photos/thumb";
+
+    public PhotoDto getPhoto(Long photoId) {
+        Optional<Photo> res = photoRepository.findById(photoId);
+        if (res.isPresent()) {
+            PhotoDto photoDto = PhotoMapper.convertToDto(res.get());
+            return photoDto;
+        } else {
+            throw new EntityNotFoundException(String.format("사진 아이디 %d로 조회되지 않았습니다.", photoId));
+        }
+    }
+
+    public PhotoDto savePhoto(MultipartFile file, Long albumId) throws IOException {
+        Optional<Album> res = albumRepository.findById(albumId);
+        if (res.isEmpty()) {
+            throw new EntityNotFoundException("앨범이 존재하지 않습니다.");
+        }
+        String fileName = file.getOriginalFilename();
+        int filesize = (int)file.getSize();
+        fileName = getNextFileName(fileName, albumId);
+        saveFile(file, albumId, fileName);
+
+        // DB에 사진 레코드 생성 & 생성된 앨범 DTO 반환
+        Photo photo = new Photo();
+        photo.setOriginalUrl("/photos/original/" + albumId + "/" + fileName);
+        photo.setThumbUrl("/photos/thumb/" + albumId + "/" + fileName);
+        photo.setFileName(fileName);
+        photo.setFileSize(filesize);
+        photo.setAlbum(res.get());
+        Photo createdPhoto = photoRepository.save(photo);
+        return PhotoMapper.convertToDto(createdPhoto);
+    }
+
+    public File getImageFile(Long photoId) {
+        Optional<Photo> res = photoRepository.findById(photoId);
+        if (res.isEmpty()) {
+            throw new EntityNotFoundException(String.format("사진을 ID %d를 찾을 수 없습니다.", photoId));
+        }
+        return new File(Constants.PATH_PREFIX + res.get().getOriginalUrl());
+    }
+
+    private String getNextFileName(String fileName, Long albumId) {
+        String fileNameNoExt = StringUtils.stripFilenameExtension(fileName);
+        String ext = StringUtils.getFilenameExtension(fileName);
+
+        Optional<Photo> res = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+
+        int count = 2;
+        // isPresent()메서드로 반환된 값이 없을때까지 실행
+        while (res.isPresent()) {
+            fileName = String.format("%s (%d).%s", fileNameNoExt, count, ext);
+            res = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+            count++;
+        }
+        return fileName;
+    }
+
+    private void saveFile(MultipartFile file, Long albumId, String fileName) throws IOException {
+        try {
+            // 원본 이미지를 original 사진 경로에 저장
+            String filePath = albumId + "/" + fileName;
+            Files.copy(file.getInputStream(), Paths.get(original_path + "/" + filePath));
+
+            // Scalr 라이브러리를 사용해서 original 이미지를 최대 300*300 사이즈로 resize 해줌
+            BufferedImage thumbImg = Scalr.resize(ImageIO.read(file.getInputStream()), Constants.THUMB_SIZE, Constants.THUMB_SIZE);
+
+            // resize된 썸네일 이미지를 저장
+            File thumbFile = new File(thumb_path + "/" + filePath);
+            String ext = StringUtils.getFilenameExtension(fileName);
+            if (ext == null) {
+                throw new IllegalArgumentException("No Extension");
+            }
+            ImageIO.write(thumbImg, ext, thumbFile);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error : " + e.getMessage());
+        }
+    }
 }
